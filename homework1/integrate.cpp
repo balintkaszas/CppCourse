375e06406f0641390c7c12093bb473fd05d4759b
Bálint Kaszás <balint.kaszas.7cb@gmail.com>
2019-03-20 03:08:38 +0100

    Updated HW1

--- homework1/integrate.cpp
+++ homework1/integrate.cpp
@@ -15,8 +15,8 @@ double integrate(int n, double x0, double x1){
     double sum2 = 0.;
 
     for(int i = 1; i <= n ; ++i){
-        sum1 = sum1 + f(x0 + step*i + step * 0.5);
-        sum2 = sum2 + f(x0 + step*i);
+        sum1 += f(x0 + step*i + step * 0.5);
+        sum2 += f(x0 + step*i);
     }
     return (step/6.)*(f(x0) + f(x1) + 4.*sum1 + 2.*sum2);
 }
@@ -24,8 +24,9 @@ double integrate(int n, double x0, double x1){
 int main(){
     std::cout.precision(16);
     const double realValue = 1.34638795680345037669816;
-    std::cout << "Integrate cos(x)*e^-x^2 from -1 to 3 (1000 steps): " << integrate(1000, -1, 3) << "\n";
-    std::cout << "Difference from real value : " << realValue - integrate(1000, -1, 3) << "\n";
+    double value = integrate(1000, -1, 3);
+    std::cout << "Integrate cos(x)*e^-x^2 from -1 to 3 (1000 steps): " << value << "\n";
+    std::cout << "Difference from real value : " << realValue - value << "\n";
     std::cout << "Convergence: \n Steps \t Value \t Difference \t";
     for(int i = 100; i < 100000; i+=100){
         std::cout << i << "\t" << integrate(i, -1, 3) << "\t" << realValue - integrate(i, -1, 3) << "\n"; 
